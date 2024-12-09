d4ace1717c9bdb8f96f090e6de17cfed0737ea39
incrusio21 <abdul.malik.ahmad997@gmail.com>
2024-12-09 13:02:45 +0000

    add(doctype): customer fund source

---
+++ it_intan_pariwara/it_intan_pariwara/doctype/customer_fund_source/customer_fund_source.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, das and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Customer Fund Source", {
+// 	refresh(frm) {
+
+// 	},
+// });
