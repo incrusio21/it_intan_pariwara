070b8de5341cc9ea9b580f0f54719503ce80cc27
incrusio21 <abdul.malik.ahmad997@gmail.com>
2025-05-05 08:41:17 +0000

    feat(new doc) : siplah order

---
+++ it_intan_pariwara/it_intan_pariwara/doctype/siplah_order/siplah_order.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, das and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class SiplahOrder(Document):
+	pass
---
+++ it_intan_pariwara/it_intan_pariwara/doctype/siplah_order/test_siplah_order.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, das and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSiplahOrder(FrappeTestCase):
+	pass
